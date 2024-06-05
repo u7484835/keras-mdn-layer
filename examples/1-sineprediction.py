9e4a49cb4d88c76fbfcc605415aac58daaa2acda
Campbell McTernan <u7484835@anu.edu.au>
2024-06-06 09:17:42 +1000

    fixed keras_mdn_layer import using trick from notebooks/context.py

--- examples/1-sineprediction.py
+++ examples/1-sineprediction.py
@@ -1,5 +1,10 @@
 # Normal imports for everybody
 from tensorflow import keras
+# Using trick from notebooks/context.py
+# little path hack to access module which is one directory up.
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import keras_mdn_layer as mdn
 import numpy as np
 
