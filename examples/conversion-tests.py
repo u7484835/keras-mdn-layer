d3b59d9220475424df737f0acf3849e65ff0d637
Campbell McTernan <u7484835@anu.edu.au>
2024-06-06 09:41:21 +1000

    small conversion test

--- examples/conversion-tests.py
+++ examples/conversion-tests.py
@@ -63,7 +63,7 @@ converter.target_spec.supported_ops = [
 tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
 tf.lite.OpsSet.SELECT_TF_OPS, # enable TensorFlow ops.   
 ]
-converter._experimental_lower_tensor_list_ops = False
+# converter._experimental_lower_tensor_list_ops = False
 
 tflite_model = converter.convert()
 tflite_model_name = '1-sineprediction-lite.tflite'
