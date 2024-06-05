5874da7a293c528e55188d6353b349010bd40fa9
Campbell McTernan <u7484835@anu.edu.au>
2024-06-06 09:34:08 +1000

    added more supported ops as suggested

--- examples/conversion-tests.py
+++ examples/conversion-tests.py
@@ -58,12 +58,12 @@ import tensorflow as tf
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
-"""converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.target_spec.supported_ops = [
 tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
 tf.lite.OpsSet.SELECT_TF_OPS, # enable TensorFlow ops.   
 ]
-converter._experimental_lower_tensor_list_ops = False"""
+converter._experimental_lower_tensor_list_ops = False
 
 tflite_model = converter.convert()
 tflite_model_name = '1-sineprediction-lite.tflite'
