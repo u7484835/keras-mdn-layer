79a4663f230c88e4502fec3d6d216ba7d9d333cc
Campbell McTernan <u7484835@anu.edu.au>
2024-06-15 21:41:44 +1000

    continuing repo clean

--- notebooksTfLite/context.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# little path hack to access module which is one directory up.
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import keras_mdn_layer as mdn
-from tensorflow import keras
